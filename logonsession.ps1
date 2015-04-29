e7a194bd827455e3ebd46daf9f24a2c8531c8c27
Craig Meinschein <cmeinsch@gmail.com>
2015-04-29 13:54:22 -0700

    Added checking for locked session.

--- logonsession.ps1
+++ logonsession.ps1
@@ -2,11 +2,21 @@
     param(
         [String]$ComputerName = $env:COMPUTERNAME
     )
-    $session = New-Object -TypeName PSCustomObject
     $online = Test-Connection -ComputerName $ComputerName -Count 1 -Quiet
     if (-not $online) { throw "Computer $ComputerName is not on." }
 
-    $session | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ComputerName.toUpper()
+    $comp = Get-WmiObject -ComputerName $ComputerName -Class Win32_ComputerSystem
+    $os = Get-WmiObject -ComputerName $ComputerName -Class Win32_OperatingSystem
+    if ($comp.UserName) {
+        $locked = if (Get-Process -Name LogonUI -ComputerName $ComputerName -ErrorAction SilentlyContinue) {$true} else {$false}
+    } else {
+        $locked = $false
+    }
+
+    $session = New-Object -TypeName PSCustomObject
+    $session | Add-Member -MemberType NoteProperty -Name ComputerName -Value $comp.__SERVER
+    $session | Add-Member -MemberType NoteProperty -Name UserName -Value $comp.UserName
+    $session | Add-Member -MemberType NoteProperty -Name Locked -Value $locked
 
     return $session
 }
