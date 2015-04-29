d9a1d91d7344a312a9db47d7c32bad586bf2437d
Craig Meinschein <cmeinsch@gmail.com>
2015-04-29 13:01:02 -0700

    Started object-oriented refactor.

---
+++ logonsession.ps1
@@ -0,0 +1,14 @@
+﻿Function LogonSessionFactory {
+    param(
+        [String]$ComputerName = $env:COMPUTERNAME
+    )
+    $session = New-Object -TypeName PSCustomObject
+    $online = Test-Connection -ComputerName $ComputerName -Count 1 -Quiet
+    if (-not $online) { throw "Computer $ComputerName is not on." }
+
+    $session | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ComputerName.toUpper()
+
+    return $session
+}
+
+if ($args) { LogonSessionFactory $args[0] } else { LogonSessionFactory }
