ca82da584f934ef9361b8390edf36f92d618e2c1
Craig Meinschein <cmeinsch@gmail.com>
2015-05-08 13:07:26 -0700

    Extended PSTS object instead of using custom one.

--- logonsession.ps1
+++ logonsession.ps1
@@ -61,27 +61,14 @@ Function LogonSessionFactory {
     $logonSessions = @()
     foreach ($ts in $tsSessions) {
         $locktime = GetSessionLockTime $comp
-
-        $session = New-Object -TypeName PSCustomObject
-        $session | Add-Member -MemberType NoteProperty -Name ComputerName -Value $comp.__SERVER
-        $session | Add-Member -MemberType NoteProperty -Name UserName -Value $ts.UserName
-        $session | Add-Member -MemberType NoteProperty -Name Type -Value $ts.WindowStationName
-        $session | Add-Member -MemberType NoteProperty -Name LockTime -Value $locktime
-        $session | Add-Member -MemberType ScriptMethod -Name Disconnect -Value {Import-Module PSTerminalServices;Get-TSSession -ComputerName $($this.ComputerName) -UserName $($this.UserName) | Disconnect-TSSession -Force}
-        $session | Add-Member -MemberType ScriptMethod -Name Logoff -Value {Import-Module PSTerminalServices;Get-TSSession -ComputerName $($this.ComputerName) -UserName $($this.UserName) | Stop-TSSession -Force}
-        if ($ts.WindowStationName -eq 'Console') {
-            # Console session
+        $ts | Add-Member -MemberType NoteProperty -Name LockTime -Value $locktime
+        $ts | Add-Member -MemberType ScriptMethod -Name Disconnect -Value {Import-Module PSTerminalServices;Get-TSSession -ComputerName $($this.ComputerName) -UserName $($this.UserName) | Disconnect-TSSession -Force} -Force
+        $ts | Add-Member -MemberType ScriptMethod -Name Logoff -Value {Import-Module PSTerminalServices;Get-TSSession -ComputerName $($this.ComputerName) -UserName $($this.UserName) | Stop-TSSession -Force} -Force
+        if (($ts.WindowStationName -eq 'Console') -and ($ts.ConnectionState -eq 'Active')) {
             $locked = if (Get-Process -Name LogonUI -ComputerName $ComputerName -ErrorAction SilentlyContinue) {$true} else {$false}
-            if ($locked) {
-                $session | Add-Member -MemberType NoteProperty -Name Status -Value "Locked"
-            } else {
-                $session | Add-Member -MemberType NoteProperty -Name Status -Value $ts.ConnectionState
-            }
-        } else {
-            # RDP or Switched-User session
-            $session | Add-Member -MemberType NoteProperty -Name Status -Value $ts.ConnectionState
         }
-        $logonSessions += $session
+        $ts | Add-Member -MemberType NoteProperty -Name Locked -Value $locked # Intentionally null for disconnected sessions
+        $logonSessions += $ts
     }
     return $logonSessions
 }
